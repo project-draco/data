f54a7874cea89996b8e037d2c2262277950f3832
Marcos César de Oliveira <mcesarhm@gmail.com>
2019-02-21 20:18:33 -0300

    Initial commit

---
+++ suggestions/s0_004c0_5/run.sh
@@ -0,0 +1,263 @@
+STATIC=../../binaries/minimal
+ASSOCIATIONRULES=../../association-rules/s0_004c0_5
+recommender --output suggestions $STATIC/actor/actor.mdg \
+    $ASSOCIATIONRULES/actor-platform.mdg /dev/null \
+    $STATIC/actor/actor-inheritance.txt > actor.txt
+recommender --output suggestions $STATIC/ambari/ambari.mdg \
+    $ASSOCIATIONRULES/ambari.mdg /dev/null \
+    $STATIC/ambari/ambari-inheritance.txt > ambari.txt
+recommender --output suggestions $STATIC/Anki-Android/Anki-Android.mdg \
+    $ASSOCIATIONRULES/Anki-Android.mdg /dev/null \
+    $STATIC/Anki-Android/Anki-Android-inheritance.txt > Anki-Android.txt
+recommender --output suggestions $STATIC/atmosphere/atmosphere.mdg \
+    $ASSOCIATIONRULES/atmosphere.mdg /dev/null \
+    $STATIC/atmosphere/atmosphere-inheritance.txt > atmosphere.txt
+recommender --output suggestions $STATIC/basex/basex.mdg \
+    $ASSOCIATIONRULES/basex.mdg /dev/null \
+    $STATIC/basex/basex-inheritance.txt > basex.txt
+recommender --output suggestions $STATIC/bazel/bazel.mdg \
+    $ASSOCIATIONRULES/bazel.mdg /dev/null \
+    $STATIC/bazel/bazel-inheritance.txt > bazel.txt
+recommender --output suggestions $STATIC/bbb/bbb.mdg \
+   $ASSOCIATIONRULES/bbb.mdg /dev/null \
+   $STATIC/bbb/bbb-inheritance.txt > bbb.txt
+recommender --output suggestions $STATIC/BroadLeafCommerce/BroadLeafCommerce.mdg \
+    $ASSOCIATIONRULES/BroadleafCommerce.mdg /dev/null \
+    $STATIC/BroadLeafCommerce/BroadLeafCommerce-inheritance.txt > BroadleafCommerce.txt
+recommender --output suggestions $STATIC/buck/buck.mdg \
+    $ASSOCIATIONRULES/buck.mdg /dev/null \
+    $STATIC/buck/buck-inheritance.txt > buck.txt
+recommender --output suggestions $STATIC/camel/camel.mdg \
+    $ASSOCIATIONRULES/camel.mdg /dev/null \
+    $STATIC/camel/camel-inheritance.txt > camel.txt
+recommender --output suggestions $STATIC/cas/cas.mdg \
+    $ASSOCIATIONRULES/cas.mdg /dev/null \
+    $STATIC/cas/cas-inheritance.txt > cas.txt
+recommender --output suggestions $STATIC/cassandra/cassandra.mdg \
+    $ASSOCIATIONRULES/cassandra.mdg /dev/null \
+    $STATIC/cassandra/cassandra-inheritance.txt > cassandra.txt
+recommender --output suggestions $STATIC/cgeo/cgeo.mdg \
+    $ASSOCIATIONRULES/cgeo.mdg /dev/null \
+    $STATIC/cgeo/cgeo-inheritance.txt > cgeo.txt
+recommender --output suggestions $STATIC/closure-compiler/closure-compiler.mdg \
+    $ASSOCIATIONRULES/closure-compiler.mdg /dev/null \
+    $STATIC/closure-compiler/closure-compiler-inheritance.txt > closure-compiler.txt
+recommender --output suggestions $STATIC/cloudstack/cloudstack.mdg \
+    $ASSOCIATIONRULES/cloudstack.mdg /dev/null \
+    $STATIC/cloudstack/cloudstack-inheritance.txt > cloudstack.txt
+recommender --output suggestions $STATIC/corenlp/corenlp.mdg \
+    $ASSOCIATIONRULES/CoreNLP.mdg /dev/null \
+    $STATIC/corenlp/corenlp-inheritance.txt > corenlp.txt
+recommender --output suggestions $STATIC/dbeaver/dbeaver.mdg \
+    $ASSOCIATIONRULES/dbeaver.mdg /dev/null \
+    $STATIC/dbeaver/dbeaver-inheritance.txt > dbeaver.txt
+recommender --output suggestions $STATIC/deeplearning4j/deeplearning4j.mdg \
+    $ASSOCIATIONRULES/deeplearning4j.mdg /dev/null \
+    $STATIC/deeplearning4j/deeplearning4j-inheritance.txt > deeplearning4j.txt
+recommender --output suggestions $STATIC/drools/drools.mdg \
+    $ASSOCIATIONRULES/drools.mdg /dev/null \
+    $STATIC/drools/drools-inheritance.txt > drools.txt
+recommender --output suggestions $STATIC/druid/druid.mdg \
+    $ASSOCIATIONRULES/druid.mdg /dev/null \
+    $STATIC/druid/druid-inheritance.txt > druid.txt
+recommender --output suggestions $STATIC/elasticsearch/elasticsearch.mdg \
+    $ASSOCIATIONRULES/elasticsearch.mdg /dev/null \
+    $STATIC/elasticsearch/elasticsearch-inheritance.txt > elasticsearch.txt
+recommender --output suggestions $STATIC/fabric8/fabric8.mdg \
+    $ASSOCIATIONRULES/fabric8.mdg /dev/null \
+    $STATIC/fabric8/fabric8-inheritance.txt > fabric8.txt
+recommender --output suggestions $STATIC/fbreaderj/fbreaderj.mdg \
+    $ASSOCIATIONRULES/FBReaderJ.mdg /dev/null \
+    $STATIC/fbreaderj/fbreaderj-inheritance.txt > fbreaderj.txt
+recommender --output suggestions $STATIC/flink/flink.mdg \
+    $ASSOCIATIONRULES/flink.mdg /dev/null \
+    $STATIC/flink/flink-inheritance.txt > flink.txt
+recommender --output suggestions $STATIC/geoserver/geoserver.mdg \
+    $ASSOCIATIONRULES/geoserver.mdg /dev/null \
+    $STATIC/geoserver/geoserver-inheritance.txt > geoserver.txt
+recommender --output suggestions $STATIC/geotools/geotools.mdg \
+    $ASSOCIATIONRULES/geotools.mdg /dev/null \
+    $STATIC/geotools/geotools-inheritance.txt > geotools.txt
+recommender --output suggestions $STATIC/gradle/gradle.mdg \
+    $ASSOCIATIONRULES/gradle.mdg /dev/null \
+    $STATIC/gradle/gradle-inheritance.txt > gradle.txt
+recommender --output suggestions $STATIC/grails/grails.mdg \
+    $ASSOCIATIONRULES/grails-core.mdg /dev/null \
+    $STATIC/grails/grails-inheritance.txt > grails.txt
+recommender --output suggestions $STATIC/groovy/groovy.mdg \
+    $ASSOCIATIONRULES/groovy.mdg /dev/null \
+    $STATIC/groovy/groovy-inheritance.txt > groovy.txt
+recommender --output suggestions $STATIC/gwt/gwt.mdg \
+    $ASSOCIATIONRULES/gwt.mdg /dev/null \
+    $STATIC/gwt/gwt-inheritance.txt > gwt.txt
+recommender --output suggestions $STATIC/h2o-3/h2o-3.mdg \
+    $ASSOCIATIONRULES/h2o-3.mdg /dev/null \
+    $STATIC/h2o-3/h2o-3-inheritance.txt > h2o-3.txt
+recommender --output suggestions $STATIC/hazelcast/hazelcast.mdg \
+    $ASSOCIATIONRULES/hazelcast.mdg /dev/null \
+    $STATIC/hazelcast/hazelcast-inheritance.txt > hazelcast.txt
+recommender --output suggestions $STATIC/hbase/hbase.mdg \
+    $ASSOCIATIONRULES/hbase.mdg /dev/null \
+    $STATIC/hbase/hbase-inheritance.txt > hbase.txt
+recommender --output suggestions $STATIC/hibernate/hibernate.mdg \
+    $ASSOCIATIONRULES/hibernate-orm.mdg /dev/null \
+    $STATIC/hibernate/hibernate-inheritance.txt > hibernate.txt
+recommender --output suggestions $STATIC/hive/hive.mdg \
+    $ASSOCIATIONRULES/hive.mdg /dev/null \
+    $STATIC/hive/hive-inheritance.txt > hive.txt
+recommender --output suggestions $STATIC/i2p/i2p.mdg \
+    $ASSOCIATIONRULES/i2p.i2p.mdg /dev/null \
+    $STATIC/i2p/i2p-inheritance.txt > i2p.txt
+recommender --output suggestions $STATIC/idea/idea.mdg \
+    $ASSOCIATIONRULES/intellij-community.mdg /dev/null \
+    $STATIC/idea/idea-inheritance.txt > idea.txt
+recommender --output suggestions $STATIC/ignite/ignite.mdg \
+    $ASSOCIATIONRULES/ignite.mdg /dev/null \
+    $STATIC/ignite/ignite-inheritance.txt > ignite.txt
+recommender --output suggestions $STATIC/isis/isis.mdg \
+    $ASSOCIATIONRULES/isis.mdg /dev/null \
+    $STATIC/isis/isis-inheritance.txt > isis.txt
+recommender --output suggestions $STATIC/jabref/jabref.mdg \
+    $ASSOCIATIONRULES/jabref.mdg /dev/null \
+    $STATIC/jabref/jabref-inheritance.txt > jabref.txt
+recommender --output suggestions $STATIC/jetty/jetty.mdg \
+    $ASSOCIATIONRULES/jetty.project.mdg /dev/null \
+    $STATIC/jetty/jetty-inheritance.txt > jetty.txt
+recommender --output suggestions $STATIC/jitsi/jitsi.mdg \
+    $ASSOCIATIONRULES/jitsi.mdg /dev/null \
+    $STATIC/jitsi/jitsi-inheritance.txt > jitsi.txt
+recommender --output suggestions $STATIC/jmeter/jmeter.mdg \
+    $ASSOCIATIONRULES/jmeter.mdg /dev/null \
+    $STATIC/jmeter/jmeter-inheritance.txt > jmeter.txt
+recommender --output suggestions $STATIC/jmonkeyengine/jmonkeyengine.mdg \
+    $ASSOCIATIONRULES/jmonkeyengine.mdg /dev/null \
+    $STATIC/jmonkeyengine/jmonkeyengine-inheritance.txt > jmonkeyengine.txt
+recommender --output suggestions $STATIC/kaa/kaa.mdg \
+    $ASSOCIATIONRULES/kaa.mdg /dev/null \
+    $STATIC/kaa/kaa-inheritance.txt > kaa.txt
+recommender --output suggestions $STATIC/keycloak/keycloak.mdg \
+    $ASSOCIATIONRULES/keycloak.mdg /dev/null \
+    $STATIC/keycloak/keycloak-inheritance.txt > keycloak.txt
+recommender --output suggestions $STATIC/killbill/killbill.mdg \
+    $ASSOCIATIONRULES/killbill.mdg /dev/null \
+    $STATIC/killbill/killbill-inheritance.txt > killbill.txt
+recommender --output suggestions $STATIC/kotlinc/kotlinc.mdg \
+    $ASSOCIATIONRULES/kotlin.mdg /dev/null \
+    $STATIC/kotlinc/kotlinc-inheritance.txt > kotlinc.txt
+recommender --output suggestions $STATIC/kylin/kylin.mdg \
+    $ASSOCIATIONRULES/kylin.mdg /dev/null \
+    $STATIC/kylin/kylin-inheritance.txt > kylin.txt
+recommender --output suggestions $STATIC/languagetool/languagetool.mdg \
+    $ASSOCIATIONRULES/languagetool.mdg /dev/null \
+    $STATIC/languagetool/languagetool-inheritance.txt > languagetool.txt
+recommender --output suggestions $STATIC/libgdx/libgdx.mdg \
+    $ASSOCIATIONRULES/libgdx.mdg /dev/null \
+    $STATIC/libgdx/libgdx-inheritance.txt > libgdx.txt
+recommender --output suggestions $STATIC/liferay-portal/liferay-portal.mdg \
+    $ASSOCIATIONRULES/liferay-portal.mdg /dev/null \
+    $STATIC/liferay-portal/liferay-portal-inheritance.txt > liferay-portal.txt
+recommender --output suggestions $STATIC/liquibase/liquibase.mdg \
+    $ASSOCIATIONRULES/liquibase.mdg /dev/null \
+    $STATIC/liquibase/liquibase-inheritance.txt > liquibase.txt
+recommender --output suggestions $STATIC/minecraftforge/minecraftforge.mdg \
+    $ASSOCIATIONRULES/MinecraftForge.mdg /dev/null \
+    $STATIC/minecraftforge/minecraftforge-inheritance.txt > minecraftforge.txt
+recommender --output suggestions $STATIC/mps/mps.mdg \
+    $ASSOCIATIONRULES/MPS.mdg /dev/null \
+    $STATIC/mps/mps-inheritance.txt > mps.txt
+recommender --output suggestions $STATIC/neo4j/neo4j.mdg \
+    $ASSOCIATIONRULES/neo4j.mdg /dev/null \
+    $STATIC/neo4j/neo4j-inheritance.txt > neo4j.txt
+recommender --output suggestions $STATIC/mule/mule.mdg \
+    $ASSOCIATIONRULES/mule.mdg /dev/null \
+    $STATIC/mule/mule-inheritance.txt > mule.txt
+recommender --output suggestions $STATIC/ofbiz/ofbiz.mdg \
+    $ASSOCIATIONRULES/ofbiz.mdg /dev/null \
+    $STATIC/ofbiz/ofbiz-inheritance.txt > ofbiz.txt
+recommender --output suggestions $STATIC/opencms/opencms.mdg \
+    $ASSOCIATIONRULES/opencms-core.mdg /dev/null \
+    $STATIC/opencms/opencms-inheritance.txt > opencms.txt
+recommender --output suggestions $STATIC/openfire/openfire.mdg \
+    $ASSOCIATIONRULES/Openfire.mdg /dev/null \
+    $STATIC/openfire/openfire-inheritance.txt > openfire.txt
+recommender --output suggestions $STATIC/openhab/openhab.mdg \
+    $ASSOCIATIONRULES/openhab.mdg /dev/null \
+    $STATIC/openhab/openhab-inheritance.txt > openhab.txt
+recommender --output suggestions $STATIC/openmrs/openmrs.mdg \
+    $ASSOCIATIONRULES/openmrs-core.mdg /dev/null \
+    $STATIC/openmrs/openmrs-inheritance.txt > openmrs.txt
+recommender --output suggestions $STATIC/opentripplanner/opentripplanner.mdg \
+    $ASSOCIATIONRULES/OpenTripPlanner.mdg /dev/null \
+    $STATIC/opentripplanner/opentripplanner-inheritance.txt > opentripplanner.txt
+recommender --output suggestions $STATIC/opentripplanner/opentripplanner.mdg \
+    $ASSOCIATIONRULES/OpenTripPlanner.mdg /dev/null \
+    $STATIC/opentripplanner/opentripplanner-inheritance.txt > opentripplanner.txt
+recommender --output suggestions $STATIC/optaplanner/optaplanner.mdg \
+    $ASSOCIATIONRULES/optaplanner.mdg /dev/null \
+    $STATIC/optaplanner/optaplanner-inheritance.txt > optaplanner.txt
+recommender --output suggestions $STATIC/orientdb/orientdb.mdg \
+    $ASSOCIATIONRULES/orientdb.mdg /dev/null \
+    $STATIC/orientdb/orientdb-inheritance.txt > orientdb.txt
+recommender --output suggestions $STATIC/osmand/osmand.mdg \
+    $ASSOCIATIONRULES/Osmand.mdg /dev/null \
+    $STATIC/osmand/osmand-inheritance.txt > osmand.txt
+recommender --output suggestions $STATIC/pentaho/pentaho.mdg \
+    $ASSOCIATIONRULES/pentaho-kettle.mdg /dev/null \
+    $STATIC/pentaho/pentaho-inheritance.txt > pentaho.txt
+recommender --output suggestions $STATIC/pinpoint/pinpoint.mdg \
+    $ASSOCIATIONRULES/pinpoint.mdg /dev/null \
+    $STATIC/pinpoint/pinpoint-inheritance.txt > pinpoint.txt
+recommender --output suggestions $STATIC/presto/presto.mdg \
+    $ASSOCIATIONRULES/presto.mdg /dev/null \
+    $STATIC/presto/presto-inheritance.txt > presto.txt
+recommender --output suggestions $STATIC/processing/processing.mdg \
+    $ASSOCIATIONRULES/processing.mdg /dev/null \
+    $STATIC/processing/processing-inheritance.txt > processing.txt
+recommender --output suggestions $STATIC/react-native/react-native.mdg \
+    $ASSOCIATIONRULES/react-native.mdg /dev/null \
+    $STATIC/react-native/react-native-inheritance.txt > react-native.txt
+recommender --output suggestions $STATIC/restlet/restlet.mdg \
+    $ASSOCIATIONRULES/restlet-framework-java.mdg /dev/null \
+    $STATIC/restlet/restlet-inheritance.txt > restlet.txt
+recommender --output suggestions $STATIC/soapui/soapui.mdg \
+    $ASSOCIATIONRULES/soapui.mdg /dev/null \
+    $STATIC/soapui/soapui-inheritance.txt > soapui.txt
+recommender --output suggestions $STATIC/solr/solr.mdg \
+    $ASSOCIATIONRULES/lucene-solr.mdg /dev/null \
+    $STATIC/solr/solr-inheritance.txt > solr.txt
+recommender --output suggestions $STATIC/sonarqube/sonarqube.mdg \
+    $ASSOCIATIONRULES/sonarqube.mdg /dev/null \
+    $STATIC/sonarqube/sonarqube-inheritance.txt > sonarqube.txt
+recommender --output suggestions $STATIC/spring-framework/spring-framework.mdg \
+    $ASSOCIATIONRULES/spring-framework.mdg /dev/null \
+    $STATIC/spring-framework/spring-framework-inheritance.txt > spring-framework.txt
+recommender --output suggestions $STATIC/storm/storm.mdg \
+    $ASSOCIATIONRULES/storm.mdg /dev/null \
+    $STATIC/storm/storm-inheritance.txt > storm.txt
+recommender --output suggestions $STATIC/structr/structr.mdg \
+    $ASSOCIATIONRULES/structr.mdg /dev/null \
+    $STATIC/structr/structr-inheritance.txt > structr.txt
+recommender --output suggestions $STATIC/terasology/terasology.mdg \
+    $ASSOCIATIONRULES/Terasology.mdg /dev/null \
+    $STATIC/terasology/terasology-inheritance.txt > terasology.txt
+recommender --output suggestions $STATIC/usergrid/usergrid.mdg \
+    $ASSOCIATIONRULES/usergrid.mdg /dev/null \
+    $STATIC/usergrid/usergrid-inheritance.txt > usergrid.txt
+recommender --output suggestions $STATIC/usergrid-java/usergrid-java.mdg \
+    $ASSOCIATIONRULES/usergrid-java.mdg /dev/null \
+    $STATIC/usergrid-java/usergrid-java-inheritance.txt > usergrid-java.txt
+recommender --output suggestions $STATIC/vaadin/vaadin.mdg \
+    $ASSOCIATIONRULES/vaadin.mdg /dev/null \
+    $STATIC/vaadin/vaadin-inheritance.txt > vaadin.txt
+recommender --output suggestions $STATIC/voltdb/voltdb.mdg \
+    $ASSOCIATIONRULES/voltdb.mdg /dev/null \
+    $STATIC/voltdb/voltdb-inheritance.txt > voltdb.txt
+recommender --output suggestions $STATIC/wicket/wicket.mdg \
+    $ASSOCIATIONRULES/wicket.mdg /dev/null \
+    $STATIC/wicket/wicket-inheritance.txt > wicket.txt
+recommender --output suggestions $STATIC/wildfly/wildfly.mdg \
+    $ASSOCIATIONRULES/wildfly.mdg /dev/null \
+    $STATIC/wildfly/wildfly-inheritance.txt > wildfly.txt
+recommender --output suggestions $STATIC/wordpress-android/wordpress-android.mdg \
+    $ASSOCIATIONRULES/WordPress-Android.mdg /dev/null \
+    $STATIC/wordpress-android/wordpress-android-inheritance.txt > wordpress-android.txt
